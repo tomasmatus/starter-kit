bd694f285989f1002981c2f7b2680a453ba8b511
tomasmatus <tomatus777@gmail.com>
2025-06-30 10:18:11 +0200

    test PR for CI

--- src/app.tsx
+++ src/app.tsx
@@ -36,7 +36,7 @@ export const Application = () => {
 
     return (
         <Card>
-            <CardTitle>Starter Kit</CardTitle>
+            <CardTitle>Starter Kit TomTest</CardTitle>
             <CardBody>
                 <Alert
                     variant="info"
